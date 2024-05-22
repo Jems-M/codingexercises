e0b4b3a2297b1af232ce3ce8e83db40f66dec4d7
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 11:01:25 +0100

    starPyramid

---
+++ starPyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace starPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printStars(6);
+        }
+
+        static void printStars(int height)
+        {
+            for (int i = 1; i < height; i++)
+            {
+                for (int j = 1; j <= height-i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
