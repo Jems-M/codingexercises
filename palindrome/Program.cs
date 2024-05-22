29da8839c874620465d536749516dce34aa9166d
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 11:32:56 +0100

    palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isPalindrome("step on no pets"));
+
+            Console.WriteLine(isPalindrome("racecar"));
+
+            Console.WriteLine(isPalindrome("book"));
+        }
+
+        static Boolean isPalindrome(string s)
+        {
+            return s == reverseString(s);
+        }
+
+        static string reverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            string output = new string(array);
+            return output;
+        }
+    }
+}
