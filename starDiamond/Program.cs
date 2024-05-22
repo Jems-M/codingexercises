4c2eb84107924ef7b6219a5a4faa7ab3f2cc21c7
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 11:22:10 +0100

    starDiamond

---
+++ starDiamond/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace starDiamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printStars(5);
+        }
+
+        static void printStars(int size)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= size; i++)
+            {
+                sb.Append(' ', size - i);
+                sb.Append('*', i * 2 - 1);
+                sb.Append("\n");
+            }
+
+            for (int i = size-1; i >= 1; i--)
+            {
+                sb.Append(' ', size - i);
+                sb.Append('*', i * 2 - 1);
+                sb.Append("\n");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
