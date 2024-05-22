ba50b48bdb9764a6bc9889c6105653b745989c3b
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 14:07:39 +0100

    string1

---
+++ String1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace String1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(makeAbba("Hi", "Bye"));
+        }
+
+        static string helloName(string name) { return "Hello " + name + "!"; }
+
+        static string makeAbba(string a, string b) { return a + b + b + a; }
+
+        static string makeTags(string tag, string word) 
+        { 
+            return "<" + tag + ">" + word + "</" + tag + ">";
+        }
+    }
+}
