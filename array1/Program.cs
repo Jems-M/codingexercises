cc60bd447e6d88d9c3aa288798959495dff890f6
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 15:33:16 +0100

    array1

---
+++ array1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace array1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(sameFirstLast([30, 12, 4, 30]));
+        }
+
+        static Boolean firstLast6(int[] array)
+        {
+            return array[0] == 6 || array[array.Length - 1] == 6;
+        }
+
+        static Boolean sameFirstLast(int[] array)
+        {
+            return array[0] == array[array.Length-1];
+        }
+
+        static int[] makePi() 
+        { 
+            return [3, 1, 4]; 
+        }
+    }
+}
