ffa991a135482b3530ae345b1b3d006ed9f95cc4
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 11:27:10 +0100

    reverseString

---
+++ stringReversal/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace stringReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static string reverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            string output = new string(array);
+            return output;
+        }
+    }
+}
