a3c061980d1af8b5d0029888618a4049687f838d
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 13:56:39 +0100

    nextPrime

---
+++ nextPrime/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace nextPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nextPrime(60));
+        }
+
+        static int nextPrime(int val)
+        {
+            val++;
+            while (!isPrime(val))
+            {
+                val++;
+            }
+
+            return val;
+
+        }
+
+        static Boolean isPrime(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
