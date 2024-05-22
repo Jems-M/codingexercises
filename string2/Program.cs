2b22b4911caf9fb8c16f9ff2b608e31d6eb6d7a6
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 14:24:10 +0100

    string2

---
+++ string2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace string2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(catDog("catdogcatdogdogcat"));
+        }
+
+        static string doubleChar(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                output = output.Insert(output.Length, input[i].ToString());
+                output = output.Insert(output.Length, input[i].ToString());
+            }
+
+            return output;
+        }
+
+        static int countHi(string input)
+        {
+            int hiCount = 0;
+            for (int i = 0; i < input.Length; i++) 
+            {
+                if (input[i] == 'h' && input[i+1] == 'i')
+                {
+                    hiCount++;
+                }
+            }
+
+            return hiCount;
+        }
+
+        static bool catDog(string input)
+        {
+            int catCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'c' && input[i + 1] == 'a' && input[i + 2] == 't')
+                {
+                    catCount++;
+                }
+            }
+
+            int dogCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'd' && input[i + 1] == 'o' && input[i + 2] == 'g')
+                {
+                    dogCount++;
+                }
+            }
+
+            return catCount == dogCount;
+        }
+    }
+}
