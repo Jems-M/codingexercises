b5bdf3df8dac9005a8284263a2542f219d96cb25
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 13:32:59 +0100

    twoSums

---
+++ twoSums/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace twoSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10));
+        }
+
+        static Tuple<int, int> findTwoSum(List<int> nums, int target)
+        {
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = 0;  j < nums.Count; j++)
+                {
+                    if (nums[j] + nums[i] == target)
+                    {
+                        return new Tuple<int, int>(i, j);
+                    }
+                }
+
+            }
+
+            return null;
+
+        }
+    }
+}
