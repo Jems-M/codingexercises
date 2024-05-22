0f6663740188c551bfe29ba866db86423146ab8a
Jems-M <83780887+Jems-M@users.noreply.github.com>
2024-05-22 13:12:27 +0100

    sumDigits

---
+++ sumDigits/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace sumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumDigits(23182));
+        }
+
+        static int sumDigits(int n)
+        {
+            char[] chars = n.ToString().ToCharArray();
+            List<int> digits = new List<int>();
+
+            foreach (char c in chars)
+            {
+                string cStr = c.ToString();
+                digits.Add(int.Parse(cStr));
+            }
+
+            return digits.Sum();
+        }
+    }
+}
